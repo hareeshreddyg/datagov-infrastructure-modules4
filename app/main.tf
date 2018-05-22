46dee99afe856e2109363b883567c3ee56a2a0e1
hareeshreddyg <hareeshg@ekuber.com>
2018-05-22 09:17:31 -0400

    added s3 backend state to app module

--- app/main.tf
+++ app/main.tf
@@ -5,6 +5,14 @@ provider "aws" {
 terraform {
   backend "s3" {}
 }
+data "terraform_remote_state" "app" {
+  backend = "s3"
+  config {
+    bucket = "datagov-terraform-state"
+    key    = "${var.env}/app/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 data "terraform_remote_state" "vpc" {
   backend = "s3"
